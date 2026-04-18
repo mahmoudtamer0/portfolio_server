c9de60d8583e75f8d9c18b99bd83220dd52e463c
mahmoud <mamoidtamer300@gmail.com>
2026-04-18 21:51:26 +0200

    dd

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import projectsRoutes from "./routers/project.router.js";
 
 const app = express();
+app.set("trust proxy", 1);
 
 app.use(cors());
 app.use(express.json());
