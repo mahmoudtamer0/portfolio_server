f5d13078dd06a9ed3f4ec72e642a689a5881d7f3
mahmoud <mamoidtamer300@gmail.com>
2026-04-18 21:59:05 +0200

    a

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,5 +1,5 @@
-import app from "../app";
-import { connectDB } from "../db";
+import app from "../app.js";
+import { connectDB } from "../db.js";
 
 export default async function handler(req, res) {
     await connectDB();
